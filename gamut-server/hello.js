f7b4ff8263ea4cc622135cbf2919039c6297f061
Howard Ricketts <molorosh@gmail.com>
2020-12-24 22:06:13 +0000

    made the node web server return HTML5

---
+++ gamut-server/hello.js
@@ -0,0 +1,30 @@
+/**
+ * Let's create a simple web server
+ * 
+ */
+
+ var http = require('http');
+
+ function process_request(req, res){
+     var body = '<!DOCTYPE html>\n';
+     body += '<html lang="en">\n';
+     body += '<head>\n';
+     body += '<meta charset="utf-8">\n';
+     body += '<title>Gamut Backend Server</title>\n';
+     body += '</head>\n';
+     body += '<body>\n';
+     body += '<h1>Gamut Backend Server</h1>\n';
+     body += '</body>\n';
+     body += '</html>\n';
+     var content_length = body.length;
+     res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8',
+        'Content-Length': content_length,
+     });
+     res.end(body);ß
+ }
+
+ var a = http.createServer(process_request);
+ a.listen(8080);
+ console.log("Listening on http://localhost:8080/");
+ 
