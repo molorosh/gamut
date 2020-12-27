3149c02f30488f3347bb37b94009e7dd2d1a719a
Howard Ricketts <molorosh@gmail.com>
2020-12-27 23:07:45 +0000

    added new REST API to the list of request handlers

--- gamut-server/runAlpha.js
+++ gamut-server/runAlpha.js
@@ -5,11 +5,14 @@
 
 var alphaServer = require('./AlphaServer.js');
 var alphaPageRequest = require('./AlphaPageRequest.js');
+var alphaRestRequest = require('./AlphaRestRequest.js');
 
 const server = new alphaServer(8080);
 server.addHandler(new alphaPageRequest('GET', '/', 'zMainPage.html', 'text/html; charset=utf-8'));
 server.addHandler(new alphaPageRequest('GET', '/style', 'zStyle.css', 'text/css'));
 server.addHandler(new alphaPageRequest('GET', '/questionMark', 'question_mark.svg', 'image/svg+xml'));
+server.addHandler(new alphaPageRequest('GET', '/appCode', 'zAppCode.js', 'text/javascript'));
+server.addHandler(new alphaRestRequest());
 // this should always be the last page request handler:
 server.addHandler(new alphaPageRequest('*', '*', 'z404.html', 'text/html; charset=utf-8'));
 server.startServer();
